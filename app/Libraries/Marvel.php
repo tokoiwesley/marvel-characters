04d3092fc224b8291a06bf3b6099cab7de506412
Wesley Tokoi <tokoiwesley@gmail.com>
2022-04-07 05:26:49 +0300

    Create a Marvel library class

--- app/Libraries/Marvel.php
+++ app/Libraries/Marvel.php
@@ -4,5 +4,39 @@ namespace App\Libraries;
 
 class Marvel
 {
+    public static function getClientParams(): string
+    {
+        return "apiKey=" . Marvel::getPublicKey();
+    }
+
+    public static function getServerParams(): string
+    {
+        $timestamp = Marvel::getTimestamp();
+        $params = "ts=" . $timestamp
+            . "&apiKey=" . Marvel::getPublicKey()
+            . "&hash=" . Marvel::getHash($timestamp);
+        return $params;
+    }
+
+    private static function getTimestamp(): string
+    {
+        return now()->getTimestamp();
+    }
+
+    private static function getHash(string $timestamp): string
+    {
+        $stringToHash = $timestamp . Marvel::getPrivateKey() . Marvel::getPublicKey();
+        return md5($stringToHash);
+    }
+
+    private static function getPublicKey(): string
+    {
+        return config('services.marvel.key');
+    }
+
+    private static function getPrivateKey(): string
+    {
+        return config('services.marvel.secret');
+    }
 
 }
