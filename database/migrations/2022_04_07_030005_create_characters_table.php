b14ba4a6892fd79157f14998ebc947e5ee459a94
Wesley Tokoi <tokoiwesley@gmail.com>
2022-04-07 06:12:12 +0300

    Create a character model, migration and factory

---
+++ database/migrations/2022_04_07_030005_create_characters_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCharactersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('characters', function (Blueprint $table) {
+            $table->id();
+            $table->integer('unique_id')->nullable();
+            $table->string("name")->nullable();
+            $table->string("description")->nullable();
+            $table->string("modified")->nullable();
+            $table->string("resource_uri")->nullable();
+            $table->json('thumbnail')->nullable();
+            $table->json('comics')->nullable();
+            $table->json('series')->nullable();
+            $table->json('stories')->nullable();
+            $table->json('events')->nullable();
+            $table->json('urls')->nullable();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('characters');
+    }
+}
